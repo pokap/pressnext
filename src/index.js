5838df21991f19f92556c533380379fc457b07c4
Florent Denis <dflorent.pokap@gmail.com>
2018-01-24 00:29:02 +0100

    aaaaaaa

--- src/index.js
+++ src/index.js
@@ -31,7 +31,9 @@ import chapters from 'data_chapter';
  *          - Petite histoire
  *              - c'est une IA (la fenêtre), une rescapée d'un guerre qu'il y a eu entre une colonni humaine et une autre armé humain
  *              - "En fait on va faire un autre histoire"
- *              - c'est l'histoire d'un cowboy qui donne une lettre à une cowboy elle ne dit rien et demande "Qu'est ce que cela veut dire ?" <- faire un son google
+ *              - Nouvel histoire : Vous être un cowboy et vous allez à la rencontre d'une cowboy pour lui donner une lettre.
+ *                  - (boite de dialogue, ce qu'on y écrit est dit au prochain level, mettre le message "get schwifty" pour un autre chapitre)
+ * (1)      - Vous lui dites ____ en lui donnant lettre, après avoir lu la lettre elle vous demande "Qu'est ce que cela veut dire ?" <- faire un son google, pas besoin de décompte
  *                  - qu'est ce que vous faite ?
  *              - elle s'approche de vous mais vous ne pouvez pas réagir vos yeux plongé dans les siens.
  *                  - qu'est ce que vous faite ?
@@ -44,27 +46,27 @@ import chapters from 'data_chapter';
  *              - elle vous tire dessus un grand tire (bruit) qui vous faits tomber du cheval, votre vie prend fin avant même de toucher le sol.
  *                  - Game over. (Reprise à la dernière sauvegarde flag-du-cycle)
  *              - "Bon si vous ne voulez pas jouer à mes histoires on faire autre chose."
+ *          - Dernier chapitre:
+ *              - Faire un jeu simon prélocalisé, si c'est fini plus vite que prévu faire un message troll
+ *              - Faire un compte-à-rebours (sinon prev) avec le next qui s'affiche au dernier moment (on peut clické dessus avant) prélocalisé.
+ *              - Plusieurs level dans lequel il faut trouvé une suite d'object pour avoir la clé du dernier next
+ *                  - Level 1
+ *                      - une première armoire bleu fermé qui contient la clé final
+ *                      - une seconde armoire marron contient un clé jaune
+ *                  - Level 2
+ *                      - un coffre fermé jaune contient un clé verte
+ *                  - Level 3
+ *                      - chapitre sous-sol
+ *                      - Level 3-1
+ *                          - une status verte
+ *                      - Level 3-2
+ *                      - Level 3-3
+ * (2)                      - une trape noir (fermé par une clé noir à trouver dans un level caché, qui renvoie faire un autre chapitre)
+ *                  - Level 4
+ *                      - une horloge de bureau (bruit)
+ *                      - le bouton next est grisé (désactivé mais non disable) avec la sérure
  *          - 
- *      - Adds a sub-game:
- *          - Faire un jeu simon prélocalisé, si c'est fini plus vite que prévu faire un message troll
- *          - Faire un compte-à-rebours (sinon prev) avec le next qui s'affiche au dernier moment (on peut clické dessus avant) prélocalisé.
- *          - Plusieurs level dans lequel il faut trouvé une suite d'object pour avoir la clé du dernier next
- *              - Level 1
- *                  - une première armoire bleu fermé qui contient la clé final
- *                  - une seconde armoire marron contient un clé jaune
- *              - Level 2
- *                  - un coffre fermé jaune contient un clé verte
- *              - Level 3
- *                  - chapitre sous-sol
- *                  - Level 3-1
- *                      - une status verte
- *                  - Level 3-2
- *                  - Level 3-3
- *                      - une trape noir fermé qui renvoie faire un autre niveau
- *              - Level 4
- *                  - une horloge de bureau (bruit)
- *                  - le bouton next est grisé (désactivé mais non disable) avec la sérure
- *          - 
+ *          - Fin (recommencer, crédit passage rapide, mettre à la fin "vous pouviez lui dire "get schwifty"" sur (1))
  */
 
 let d_background;
